2e9d16876c2ee4e0d783b6776fb6db066a650bcc
Dalrae03 <www298@naver.com>
2024-02-23 18:25:32 +0900

    supervisor, helmet 추가

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const helmet = require('helmet')
 const app = express()
+app.use(helmet())
 
 // 주소를 만들어주는 도구 get
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.get('/about', function (req, res) {
+  res.send('About Page')
+})
+
+app.listen(3000, function(req,res){
+  console.log("서버 실행 중")
+})
