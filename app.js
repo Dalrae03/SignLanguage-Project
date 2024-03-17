3cd73f0d42d37463bfa0ed0ae4ade5ed070200a8
Dalrae03 <www298@naver.com>
2024-03-17 23:52:14 +0900

    메인페이지 구현중

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+/*
 const ejs = require('ejs')
 
+//ejs사용하려면 써야하는 것들
 app.set('view engine', 'ejs'); //그림파일을 전달을 할때 어떠한 뷰파일을 전달해주는 엔진(도구)를 사용할건지 명시 - 그 도구가 ejs
 // 그림만 전달하는게 아니라 데이터 전달까지 동시에 할 수 있다.
 // 그림파일 확장자 명이 ejs여야한다... 흠...
 app.set('views', './views') //내가만든 html파일은 전부 views파일에 있다. 알려줌
 app.use('/public', express.static(__dirname + '/public')); //css나 이미지 같은 정적인 파일들, 화면을 그릴 때 필요한(사용되는) 도구들 위치
 //express.static(__dirname - 퍼블릭 폴더가 app.js 폴더로부터 어디에 있는지 상대적인 주소를 나타내는 도구
+*/
 
 app.use(helmet());
 app.use(express.json()); //post방식 api사용시 필요한 설정
